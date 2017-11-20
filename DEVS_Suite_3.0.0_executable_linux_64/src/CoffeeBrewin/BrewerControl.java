1a9c47ee33a33d0bf69aff3a17207c4b2d34e0ab
ndebuhr <ndebuhr@protonmail.com>
2017-11-20 16:42:53 -0500

    Refine parameters for easier testing

--- DEVS_Suite_3.0.0_executable_linux_64/src/CoffeeBrewin/BrewerControl.java
+++ DEVS_Suite_3.0.0_executable_linux_64/src/CoffeeBrewin/BrewerControl.java
@@ -24,7 +24,7 @@ public class BrewerControl extends ViewableAtomic {
     protected double max_safe_temp;
 
     public BrewerControl() {
-	this("BrewerControl", 1, 12, 3, 5, 200);
+	this("BrewerControl", 1, 30, 2, 12, 200);
     }
 
     public BrewerControl(String name, double Register_time, double Safety_timeout,
